b5943acb3ddac676968894d00fe72a74721b3e9e
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-30 08:09:17 +0100

    Fixed bug

--- main.py
+++ main.py
@@ -25,13 +25,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-gravatar = Gravatar(app,
-                    size=100,
-                    rating='g',
-                    default='retro',
-                    force_default=False,
-                    force_lower=False,
-                    use_ssl=False,
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False,use_ssl=False,
                     base_url=None)
 
 
@@ -225,4 +219,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
